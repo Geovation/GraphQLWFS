9fd0e4ef603e9351e84ec1c524b6e1724859bdb0
Sebastian Ovide <sebastian.ovide@geovation.uk>
2019-11-05 10:53:10 +0000

    hello world (#4)

---
+++ notes.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# google component
+gcloud components install beta
+gcloud components update
+
+# python env
+python -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install --upgrade -r requirements.txt
+# pip install --upgrade google-cloud-storage
+# pip install --upgrade pytest
+
+# deploy
+# gcloud functions deploy graphqlwfs --runtime python37 --trigger-http
+
+# save packages
+# pip freeze > requirements.txt
