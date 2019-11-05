10d8c9e1925f3d2ab7c41201f9544f0a14e386a1
Sebastian <sebastian.ovide@gmail.com>
2019-11-05 15:10:36 +0000

    separating GC from local

---
+++ local.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+from main import graphqlwfs
+
+# the following code is tu run it locally. Just run "local.sh"
+app = Flask(__name__)
+@app.route('/')
+def local():
+    return graphqlwfs(request)
+
+if __name__ == '__main__':
+    app.run()
