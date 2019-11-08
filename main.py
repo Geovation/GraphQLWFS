6eb1dc61acae594d70a09eed68a41e6ab2541d45
Johann <38230021+jrajak10@users.noreply.github.com>
2019-11-08 10:59:50 +0000

    updated function to return json (#14)
    * updated function, returns json
    
    * added function, which returns (nearly) geoJson
    
    * Update .gitignore
    
    Co-Authored-By: Sebastian Ovide <sebastian.ovide@gmail.com>
    
    * removed GQL folder
    
    * removed additional requirements

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import requests
 from flask import escape
 
+
+
 def graphqlwfs(request):
+    response = requests.get('https://www.mapping.cityoflondon.gov.uk/arcgis/services/INSPIRE/MapServer/WFSServer?request=GetFeature&service=WFS&typename=INSPIRE:Licensed_Premises&outputFormat=GEOJSON&count=2')
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
@@ -10,13 +14,26 @@ def graphqlwfs(request):
         Response object using `make_response`
         <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
     """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+    # request_json = request.get_json(silent=True)
+    # request_args = request.args
 
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return 'Hello {}!'.format(escape(name))
+    # if request_json and 'name' in request_json:
+    #     name = request_json['name']
+    # elif request_args and 'name' in request_args:
+    #     name = request_args['name']
+    # else:
+    #     name = 'World'
+    return response.json()
+    
+    
+    
+    # response = requests.get('https://api.github.com')
+    # if response:
+    #     print('Success!')
+    # else:
+    #     print('An error has occurred.')
+
+    # def requestInputs(url):
+    #     pass
+
+# 'Hello {}! '.format(escape(name)) + 
