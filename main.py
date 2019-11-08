58b6a44049b9eaa764580a02959b8b6828ce5088
Johann.Rajakarunanayake <jrajakarunanayake@gmail.com>
2019-11-08 10:58:59 +0000

    function

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ def graphqlwfs(request):
         Response object using `make_response`
         <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
     """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+    # request_json = request.get_json(silent=True)
+    # request_args = request.args
 
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
+    # if request_json and 'name' in request_json:
+    #     name = request_json['name']
+    # elif request_args and 'name' in request_args:
+    #     name = request_args['name']
+    # else:
+    #     name = 'World'
     return response.json()
     
     
