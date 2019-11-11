0f3b7492eec327553abb4ebff5abfa677730fec7
Johann.Rajakarunanayake <jrajakarunanayake@gmail.com>
2019-11-11 10:08:13 +0000

    updated function with OS link as parameter

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import requests
 from flask import escape
+import json 
 
 
 
-def graphqlwfs(request):
-    response = requests.get('https://www.mapping.cityoflondon.gov.uk/arcgis/services/INSPIRE/MapServer/WFSServer?request=GetFeature&service=WFS&typename=INSPIRE:Licensed_Premises&outputFormat=GEOJSON&count=2')
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
-    """
+def graphqlwfs(url):
+    url = "https://osdatahubapi.os.uk/OSFeaturesAPI/wfs/v1?service=wfs&request=GetFeature&typename=osfeatures:BoundaryLine_CommunityWard&key=pxKGVMtaA9X2382DdJA4h3hAi6mkXt60&version=2.0.0&outputformat=geoJSON"
+    # &outputFormat=GEOJSON
+    response = requests.get(url+ "&outputFormat=GEOJSON")
+    # """HTTP Cloud Function.
+    # Args:
+    #     request (flask.Request): The request object.
+    #     <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    # Returns:
+    #     The response text, or any set of values that can be turned into a
+    #     Response object using `make_response`
+    #     <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    # """
     # request_json = request.get_json(silent=True)
     # request_args = request.args
 
@@ -23,9 +26,9 @@ def graphqlwfs(request):
     #     name = request_args['name']
     # else:
     #     name = 'World'
-    return response.json()
-    
-    
+    return response.json()["features"][0]
+
+
     
     # response = requests.get('https://api.github.com')
     # if response:
