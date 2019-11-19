f62e3b48b6843064205a8fcfd838c9b6755bb395
Johann.Rajakarunanayake <jrajakarunanayake@gmail.com>
2019-11-19 16:07:54 +0000

    started json requests on the server

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from flask import Flask, escape, request 
 import requests
-# from flask import escape
 import json 
 
 
@@ -7,28 +7,51 @@ import json
 def graphqlwfs(url):
     url = "https://osdatahubapi.os.uk/OSFeaturesAPI/wfs/v1?service=wfs&request=GetCapabilities&key=pxKGVMtaA9X2382DdJA4h3hAi6mkXt60&version=2.0.0"
     queryString = "&typenames=osfeatures:BoundaryLine_PollingDistrict&outputformat=geoJSON"
-    count = 100
-    property = "Ward"
-    propertyValue = "Ely North Ward"
 
+
+   
+
+   
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    
+    if request_json and 'count' in request_json:
+        count = request_json['count']
+    elif request_args and 'count' in request_args:
+        count = request_args['count']
+    else:
+        count = 4000
+
+    if request_json and 'property' in request_json:
+        property = request_json['property']
+    elif request_args and 'property' in request_args:
+        property = request_args['property']
+    else:
+        property = ""
+
+    if request_json and 'propertyValue' in request_json:
+        propertyValue = request_json['propertyValue']
+    elif request_args and 'propertyValue' in request_args:
+        propertyValue = request_args['propertyValue']
+    else:
+        propertyValue = ""
+
+    
     filterString = "&filter=<Filter><PropertyIsEqualTo><PropertyName>" + str(property) + "</PropertyName><Literal>" + str(propertyValue) + "</Literal></PropertyIsEqualTo></Filter>"
 
+    if property == "":
+        filterString = ""
+    if propertyValue == "":
+        filterString = ""
     
     
-    newUrl = str(url.replace("GetCapabilities", "GetFeature") + queryString + "&count=" + str(count)+ filterString)
+    newUrl = str(url.replace("GetCapabilities", "GetFeature") + queryString + "&count=" + str(count) + filterString)
     response = requests.get(newUrl)
 
     features = response.json()
 
-    return features
-            
-      
-        
-
-            
-
-
-    # """HTTP Cloud Function.
+    #  """HTTP Cloud Function.
     # Args:
     #     request (flask.Request): The request object.
     #     <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
@@ -37,14 +60,16 @@ def graphqlwfs(url):
     #     Response object using `make_response`
     #     <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
     # """
-    # request_json = request.get_json(silent=True)
-    # request_args = request.args
+    
 
-    # if request_json and 'name' in request_json:
-    #     name = request_json['name']
-    # elif request_args and 'name' in request_args:
-    #     name = request_args['name']
-    # else:
-    #     name = 'World'
 
+    return features
+
+      
+        
+
+            
+
+
+   
     
