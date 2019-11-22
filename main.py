66431e7596cac9dd2a07d4ba9af6346dd5233819
Johann.Rajakarunanayake <jrajakarunanayake@gmail.com>
2019-11-22 14:38:49 +0000

    function gets parameters from requests

--- main.py
+++ main.py
@@ -1,50 +1,43 @@
 import requests
-# from flask import escape
-import json 
-
-
-
-def graphqlwfs(url):
-    url = "https://osdatahubapi.os.uk/OSFeaturesAPI/wfs/v1?service=wfs&request=GetCapabilities&key=pxKGVMtaA9X2382DdJA4h3hAi6mkXt60&version=2.0.0"
-    queryString = "&typenames=osfeatures:BoundaryLine_PollingDistrict&outputformat=geoJSON"
-    count = 100
-    property = "Ward"
-    propertyValue = "Ely North Ward"
-
-    filterString = "&filter=<Filter><PropertyIsEqualTo><PropertyName>" + str(property) + "</PropertyName><Literal>" + str(propertyValue) + "</Literal></PropertyIsEqualTo></Filter>"
-
-    
-    
-    newUrl = str(url.replace("GetCapabilities", "GetFeature") + queryString + "&count=" + str(count)+ filterString)
-    response = requests.get(newUrl)
-
-    features = response.json()
-
-    return features
-            
-      
-        
-
-            
-
-
-    # """HTTP Cloud Function.
-    # Args:
-    #     request (flask.Request): The request object.
-    #     <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
-    # Returns:
-    #     The response text, or any set of values that can be turned into a
-    #     Response object using `make_response`
-    #     <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
-    # """
+"""HTTP Cloud Function.
+Args:
+    request (flask.Request): The request object.
+    <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+Returns:
+    The response text, or any set of values that can be turned into a
+    Response object using `make_response`
+    <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+"""
+def graphqlwfs(request):
+    wfsApiBaseUrl = "https://osdatahubapi.os.uk/OSFeaturesAPI/wfs/v1?service=wfs&request=GetFeature&key=pxKGVMtaA9X2382DdJA4h3hAi6mkXt60&version=2.0.0&outputformat=geoJSON"
     # request_json = request.get_json(silent=True)
-    # request_args = request.args
-
-    # if request_json and 'name' in request_json:
-    #     name = request_json['name']
-    # elif request_args and 'name' in request_args:
-    #     name = request_args['name']
-    # else:
-    #     name = 'World'
-
-    
+    typeNames = request.args.get("typeNames", default="")
+    count = request.args.get("count", default=100)
+    PropertyName = request.args.get("PropertyName", default="")
+    propertyValue = request.args.get("propertyValue", default="")
+    payload = {
+        'typeNames': typeNames,
+        'count': count
+    }
+    if property == "" or propertyValue == "":
+        filter = """
+            <Filter>
+                <PropertyIsEqualTo>
+                    <PropertyName>{0}</PropertyName>
+                    <Literal>{1}</Literal>
+                </PropertyIsEqualTo>
+            </Filter>
+        """.format(PropertyName, propertyValue)
+        payload["filter"] = filter
+    response = requests.get(wfsApiBaseUrl, params=payload)
+    print(">>>>>>>>>>>>>>>> payload", payload)
+    print(">>>>>>>>>>>>>>>> url", response.url)
+    print(">>>>>>>>>>>>>>>> text", response.text)
+    print(">>>>>>>>>>>>>>>> headers", response.headers)
+    print(">>>>>>>>>>>>>>>> status_code", response.status_code)
+    if status_code != 200:
+        // TODO parse XML which is in response.text
+        return "NO NO NOc ccccc"
+    else:
+        features = response.json()
+        return features
