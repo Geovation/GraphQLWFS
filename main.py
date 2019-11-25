cac97b21c3cefe3137540c1777438eac275954a7
Sebastian Ovide <sebastian.ovide@geovation.uk>
2019-11-25 11:08:33 +0000

    Parameters (#22)
    * function gets parameters from requests
    
    * function returns value when you don't filter by properties

--- main.py
+++ main.py
@@ -1,50 +1,50 @@
 import requests
-# from flask import escape
-import json 
 
-
-
-def graphqlwfs(url):
-    url = "https://osdatahubapi.os.uk/OSFeaturesAPI/wfs/v1?service=wfs&request=GetCapabilities&key=pxKGVMtaA9X2382DdJA4h3hAi6mkXt60&version=2.0.0"
-    queryString = "&typenames=osfeatures:BoundaryLine_PollingDistrict&outputformat=geoJSON"
-    count = 100
-    property = "Ward"
-    propertyValue = "Ely North Ward"
-
-    filterString = "&filter=<Filter><PropertyIsEqualTo><PropertyName>" + str(property) + "</PropertyName><Literal>" + str(propertyValue) + "</Literal></PropertyIsEqualTo></Filter>"
-
-    
-    
-    newUrl = str(url.replace("GetCapabilities", "GetFeature") + queryString + "&count=" + str(count)+ filterString)
-    response = requests.get(newUrl)
-
-    features = response.json()
-
-    return features
-            
-      
-        
-
-            
-
-
-    # """HTTP Cloud Function.
-    # Args:
-    #     request (flask.Request): The request object.
-    #     <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
-    # Returns:
-    #     The response text, or any set of values that can be turned into a
-    #     Response object using `make_response`
-    #     <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
-    # """
+"""HTTP Cloud Function.
+Args:
+    request (flask.Request): The request object.
+    <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+Returns:
+    The response text, or any set of values that can be turned into a
+    Response object using `make_response`
+    <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+"""
+def graphqlwfs(request):
+    wfsApiBaseUrl = "https://osdatahubapi.os.uk/OSFeaturesAPI/wfs/v1?service=wfs&request=GetFeature&key=pxKGVMtaA9X2382DdJA4h3hAi6mkXt60&version=2.0.0&outputformat=geoJSON"
     # request_json = request.get_json(silent=True)
-    # request_args = request.args
+    typeNames = request.args.get("typeNames", default="osfeatures:BoundaryLine_PollingDistrict")
+    count = request.args.get("count", default=100)
+    propertyName = request.args.get("propertyName", default=None)
+    propertyValue = request.args.get("propertyValue", default=None)
+    payload = {
+        'typeNames': typeNames,
+        'count': count,
+        'propertyName': propertyName,
+        "propertyValue": propertyValue
+    }
+    if propertyName == "" or propertyValue == "":
+        filter = """
+                <Filter>
+                    <PropertyIsEqualTo>
+                        <propertyName>{0}</propertyName>
+                        <Literal>{1}</Literal>
+                    </PropertyIsEqualTo>
+                </Filter>
+            """.format(propertyName, propertyValue)
+        payload["filter"] = filter
+    response = requests.get(wfsApiBaseUrl, params=payload)
+    payloader = print(">>>>>>>>>>>>>>>> payload", payload)
+    urlResponse = print(">>>>>>>>>>>>>>>> url", response.url)
+    txtResponse = print(">>>>>>>>>>>>>>>> text", response.text)
+    headerResp = print(">>>>>>>>>>>>>>>> headers", response.headers)
+    statusResp = print(">>>>>>>>>>>>>>>> status_code", response.status_code)
+    if response.status_code != 200:
+        return "Please enter a typeName!!! " + str(urlResponse) + str(txtResponse) + str(headerResp) + str(propertyName) + str(propertyValue) +str(payload) 
+    else:
+        features = response.json()
+    return features
 
-    # if request_json and 'name' in request_json:
-    #     name = request_json['name']
-    # elif request_args and 'name' in request_args:
-    #     name = request_args['name']
-    # else:
-    #     name = 'World'
-
-    
+    # if status_code != 200:
+    #     return "NOOOOOO!!!"
+    # #     return response.json()
+    # return response.json()
