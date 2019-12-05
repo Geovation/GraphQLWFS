5223ad253eaddb0e727a0e9c2fc63297df421352
Sebastian Ovide <sebastian.ovide@gmail.com>
2019-12-05 15:45:37 +0000

    hello query using generig WFS function

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import requests
 import os
 import graphene
-
-
-def fetchFeaturesFromWFS(count, typeNames, propertyName, literal):
+def fetchFeaturesFromWFS(count, typeNames, filters):
     OS_KEY = os.getenv('OS_KEY', '????????')
     wfsApiBaseUrl = "https://osdatahubapi.os.uk/OSFeaturesAPI/wfs/v1?service=wfs&request=GetFeature&key={}&version=2.0.0&outputformat=geoJSON".format(OS_KEY)
-
     payload = {
         'typeNames': typeNames,
         'count': count
     }
-    if propertyName != "" and literal != "":
-        filter = """
-                <Filter>
-                    <PropertyIsEqualTo>
-                        <PropertyName>{0}</PropertyName>
-                        <Literal>{1}</Literal>
-                    </PropertyIsEqualTo>
-                </Filter>
-            """.format(propertyName, literal)
+    # TODO: generate <Filter> from filters
+
+
+
+    propertyIsEqualTo = ""
+
+    for propertyName, literal in filters.items():
+        propertyIsEqualTo += """
+            <PropertyIsEqualTo>
+                <PropertyName>{0}</PropertyName>
+                <Literal>{1}</Literal>
+            </PropertyIsEqualTo>
+        """.format(propertyName, literal)
+
+    if propertyIsEqualTo != "":
+        filter = "<Filter>" + propertyIsEqualTo + "</Filter>"
+
+
         payload["filter"] = filter
     response = requests.get(wfsApiBaseUrl, params=payload)
-    
     if response.status_code != 200:
         payloader = print(">>>>>>>>>>>>>>>> payload", payload)
         urlResponse = print(">>>>>>>>>>>>>>>> url", response.url)
@@ -30,10 +35,15 @@ def fetchFeaturesFromWFS(count, typeNames, propertyName, literal):
         headerResp = print(">>>>>>>>>>>>>>>> headers", response.headers)
         statusResp = print(">>>>>>>>>>>>>>>> status_code", response.status_code)
         return "Error: Check your logs" 
-        
     return response.json()['features']
-
 # Getting started with GraphQL. In this way we can extract data from the query. 
+# {
+#     boundaryLinePollingDistrict (
+#         first: 5
+#         ward: "Bottisham Ward",
+#         parish: "Brinkley CP"  
+#     )
+# }
 # TODO: Next step is to convert this in a proper query.
 class Query(graphene.ObjectType):
   hello = graphene.String(
@@ -42,18 +52,24 @@ class Query(graphene.ObjectType):
       propertyName=graphene.String(default_value=""), 
       literal=graphene.String(default_value="")
   )   
-
   boundaryLinePollingDistrict = graphene.String( 
       first=graphene.Int(default_value=10),
-      ward=graphene.String(default_value="Bottisham Ward")
+      ward=graphene.String(default_value="Bottisham Ward"),
+      parish=graphene.String(default_value="Brinkley CP")
   )
-
   def resolve_hello(self, info, count, typeNames, propertyName, literal):
-    return fetchFeaturesFromWFS(count, typeNames, propertyName, literal)
+    #    TODO
+    filters = {
+    }
+    filters[propertyName] = literal
+    return fetchFeaturesFromWFS(count, typeNames, filters)
 
-  def resolve_boundaryLinePollingDistrict(self, info, first, ward):
-    return  fetchFeaturesFromWFS(count=first, typeNames="osfeatures:BoundaryLine_PollingDistrict", propertyName="ward", literal=ward) 
-
+  def resolve_boundaryLinePollingDistrict(self, info, first, ward, parish):
+    filters = {
+        "ward": ward,
+        "parish": parish
+    }  
+    return  fetchFeaturesFromWFS(count=first, typeNames="osfeatures:BoundaryLine_PollingDistrict", filters=filters) 
 """HTTP Cloud Function.
 Args:
     request (flask.Request): The request object.
@@ -65,10 +81,19 @@ Returns:
 """
 def graphqlwfs(request):
     graphQlQuery = request.data.decode('utf-8')
-
     schema = graphene.Schema(query=Query)
+
+    graphQlQuery = '''
+    { 
+         hello(
+            count: 5, 
+            propertyName: "Ward", 
+            literal: "Bottisham Ward",
+            typeNames: "osfeatures:BoundaryLine_PollingDistrict"
+        ) 
+    }
+    '''
+
     result = schema.execute(graphQlQuery)
-
     #  TODO: error handling
-
     return result.data
