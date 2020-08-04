040d85f4ac163195db8b43f5bfc5d458cbc7f20d
aymar <aymbjr@gmail.com>
2020-08-04 16:56:31 +0100

    Test local() method

--- local.py
+++ local.py
@@ -1,11 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, json
 from main import graphqlwfs
 
 # the following code is tu run it locally. Just run "local.sh"
 app = Flask(__name__)
 @app.route('/', methods = ['POST', 'GET'])
 def local():
-    return graphqlwfs(request)
+    # return graphqlwfs(request)
+    return "Yes Yes"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import graphene
 def fetchFeaturesFromWFS(count, typeNames, filters):
     OS_KEY = os.getenv('OS_KEY', '????????')
-    wfsApiBaseUrl = "https://osdatahubapi.os.uk/OSFeaturesAPI/wfs/v1?service=wfs&request=GetFeature&key={}&version=2.0.0&outputformat=geoJSON".format(OS_KEY)
+    wfsApiBaseUrl = "https://api.os.uk/features/v1/wfs?service=wfs&request=GetFeature&key={}&version=2.0.0&outputformat=geoJSON".format(OS_KEY)
     payload = {
         'typeNames': typeNames,
         'count': count
@@ -29,66 +29,66 @@ def fetchFeaturesFromWFS(count, typeNames, filters):
         txtResponse = print(">>>>>>>>>>>>>>>> text", response.text)
         headerResp = print(">>>>>>>>>>>>>>>> headers", response.headers)
         statusResp = print(">>>>>>>>>>>>>>>> status_code", response.status_code)
-        return "Error: Check your logs" 
+        return "Error: Check your logs"
     return response.json()['features']
-# Getting started with GraphQL. In this way we can extract data from the query. 
+# Getting started with GraphQL. In this way we can extract data from the query.
 # TODO: Next step is to convert this in a proper query.
 class Query(graphene.ObjectType):
     hello = graphene.String(
         count=graphene.Int(default_value=10),
-        typeNames=graphene.String(default_value="osfeatures:BoundaryLine_PollingDistrict"), 
-        propertyName=graphene.String(default_value=""), 
+        typeNames=graphene.String(default_value="osfeatures:BoundaryLine_PollingDistrict"),
+        propertyName=graphene.String(default_value=""),
         literal=graphene.String(default_value="")
-    )   
-    boundaryLinePollingDistrict = graphene.String( 
+    )
+    boundaryLinePollingDistrict = graphene.String(
         first=graphene.Int(default_value=10),
         ward=graphene.String(default_value="Bottisham Ward"),
         parish=graphene.String(default_value="Brinkley CP")
     )
-    zoomstackNames = graphene.String( 
+    zoomstackNames = graphene.String(
         first=graphene.Int(default_value=10),
         Name1=graphene.String(default_value="BRECON BEACONS NATIONAL PARK")
     )
 
-    #   { 
+    #   {
     #      hello(
-    #         count: 5, 
-    #         propertyName: "Ward", 
+    #         count: 5,
+    #         propertyName: "Ward",
     #         literal: "Bottisham Ward",
     #         typeNames: "osfeatures:BoundaryLine_PollingDistrict"
-    #     ) 
+    #     )
     # }
     def resolve_hello(self, info, count, typeNames, propertyName, literal):
         filters = {}
         filters[propertyName] = literal
         return fetchFeaturesFromWFS(count, typeNames, filters)
 
-    # { 
+    # {
     #      boundaryLinePollingDistrict(
-    #         first: 5, 
+    #         first: 5,
     #         ward: "Bottisham Ward",
     #         parish: "Burrough Green CP"
-    #     ) 
+    #     )
     # }
 
     def resolve_boundaryLinePollingDistrict(self, info, first, ward, parish):
         filters = {
             "ward": ward,
             "parish": parish
-        }  
-        return  fetchFeaturesFromWFS(count=first, typeNames="osfeatures:BoundaryLine_PollingDistrict", filters=filters) 
+        }
+        return  fetchFeaturesFromWFS(count=first, typeNames="osfeatures:BoundaryLine_PollingDistrict", filters=filters)
 
-    #  {
-    #      zoomstackNames(
-    #          first: 5,
-    #          Type: "National Park"
-    #      )
-    #  }
+     # {
+     #     zoomstackNames(
+     #         first: 5,
+     #         Type: "National Park"
+     #     )
+     # }
     def resolve_zoomstackNames(self, info, first, Name1):
         filters = {
             "Name1": Name1
-        }  
-        return  fetchFeaturesFromWFS(count=first, typeNames="osfeatures:Zoomstack_Names", filters=filters) 
+        }
+        return  fetchFeaturesFromWFS(count=first, typeNames="osfeatures:Zoomstack_Names", filters=filters)
 
 """HTTP Cloud Function.
 Args:
@@ -105,4 +105,4 @@ def graphqlwfs(request):
     result = schema.execute(graphQlQuery)
 
     #  TODO: error handling
-    return result.data
+    return str(result.data)
