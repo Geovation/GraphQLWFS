a6b07d311f007253b0635e0724cf3f7e41dd115b
Abdul Omar Faruq <abdulomarfaruq@gmail.com>
2020-12-08 13:21:28 +0000

    removed hard coded OS_KEY

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import graphene
 
 def fetchFeaturesFromWFS(count, typeNames, filters):
     # Defaults to given key value if OS_KEY cannot be accepted from environment variable.
-    OS_KEY = os.getenv('OS_KEY', '59vP4agMAycoI32XMjA6wzhBMx3QybCf')
+    OS_KEY = os.getenv('OS_KEY', '???????')
     # Edit WFS API Endpoint address here
     wfsApiBaseUrl = "https://api.os.uk/features/v1/wfs?service=wfs&request=GetFeature&key={}&version=2.0.0&outputformat=geoJSON".format(
         OS_KEY)
