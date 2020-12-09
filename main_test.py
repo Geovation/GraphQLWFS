4b3a402d14cfa7808e87a87c5c87b3e9d6231de0
Abdul Omar Faruq <abdulomarfaruq@gmail.com>
2020-12-09 13:31:29 +0000

    added test_hello and corrected to local.py

--- main_test.py
+++ main_test.py
@@ -1,24 +1,20 @@
 import unittest
+import graphene
+from flask import Flask, request
+from main import graphqlwfs, Query
+from graphene.test import Client
 
 # Need this two lines to read .env
 from dotenv import load_dotenv
 load_dotenv()
 
-class TestTest(unittest.TestCase):
-
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
-
-  def test_isupper(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
-
-  def test_split(self):
-      s = 'hello world'
-      self.assertEqual(s.split(), ['hello', 'world'])
-      # check that s.split fails when the separator is not a string
-      with self.assertRaises(TypeError):
-          s.split(2)
-
+def test_hello(self):
+  
+    schema = graphene.Schema(query=Query)
+    client = Client(schema)
+    query = ' { hello(count: 5, propertyName: "Type", literal: "Education") } '
+    executed = client.execute(query)
+    assert executed != {'data': {'hello': 'Error: Check your logs'}}
+        
 if __name__ == '__main__':
     unittest.main()
