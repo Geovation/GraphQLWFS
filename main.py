85a43f1d2491ec6412a12d8c98ff7e1e82725fef
Abdul Omar Faruq <18485958+mysticetii@users.noreply.github.com>
2021-01-29 09:42:54 +0000

    Added topographyTopographicArea API (#100)
    * Added topographyTopographicArea API
    
    * code shrinking
    
    * refactored buildWFSQuery
    
    * refactored

--- main.py
+++ main.py
@@ -3,27 +3,95 @@ import os
 import graphene
 import json
 
-
+def buildEqualToFilter(propertyName, literal):
+    propertyIsEqualTo = """
+                    <PropertyIsEqualTo>
+                        <PropertyName>{0}</PropertyName>
+                        <Literal>{1}</Literal>
+                    </PropertyIsEqualTo>
+                """.format(propertyName, literal)
+    return propertyIsEqualTo
+    
 def buildWFSQuery(count, typeNames, filters):
     payload = {
         'typeNames': typeNames,
         'count': count
     }
-
+    
     if filters:
         # filters dictionary not empty
-        propertyIsEqualTo = ""
-        for propertyName, literal in filters.items():
-            propertyIsEqualTo += """
-                <PropertyIsEqualTo>
-                    <PropertyName>{0}</PropertyName>
-                    <Literal>{1}</Literal>
-                </PropertyIsEqualTo>
-            """.format(propertyName, literal)
+        if (typeNames == "osfeatures:Zoomstack_Sites"):
+            propertyIsEqualTo = ""
+            for propertyName, literal in filters.items():
+                propertyIsEqualTo += buildEqualToFilter(propertyName=propertyName, literal=literal)
 
-        if propertyIsEqualTo != "":
-            filter = "<Filter>" + propertyIsEqualTo + "</Filter>"
-            payload["filter"] = filter
+            if propertyIsEqualTo != "":
+                payload["filter"] = "<Filter>" + propertyIsEqualTo + "</Filter>"
+        
+        elif (typeNames == "osfeatures:Zoomstack_Names"):
+            propertyIsEqualTo = ""
+            for propertyName, literal in filters.items():
+                propertyIsEqualTo += buildEqualToFilter(propertyName=propertyName, literal=literal) 
+
+            if propertyIsEqualTo != "":
+                payload["filter"] = "<Filter>" + propertyIsEqualTo + "</Filter>"
+
+        elif (typeNames == "osfeatures:Topography_TopographicArea"):
+            propertyIsEqualToList = ["", "", "", "", "", "", "", ""]
+            propertyList = [False, False, False, False, False, False, False, False]
+            multipleProperty = ""
+            multiplePropertyCount = 0
+
+            for element in filters:
+                if (filters[element] != None):
+                    
+                    if (element == "TOID" or element == "theme" or element == "reasonForChange" or element == "descriptiveGroup" or element == "make"):
+                        if (len(filters[element].strip()) != 0):
+                            if (element == "TOID"):
+                                propertyList[0] = True
+                                propertyIsEqualToList.insert( 0, buildEqualToFilter(propertyName="TOID", literal=filters['TOID']) )
+
+                            elif (element == "theme"):
+                                propertyList[2] = True
+                                propertyIsEqualToList.insert( 2, buildEqualToFilter(propertyName="theme", literal=filters['theme']) )
+                                
+                            elif (element == "reasonForChange"):
+                                propertyList[4] = True
+                                propertyIsEqualToList.insert( 4, buildEqualToFilter(propertyName="reasonForChange", literal=filters['reasonForChange']) )
+                                
+                            elif (element == "descriptiveGroup"):
+                                propertyList[5] = True
+                                propertyIsEqualToList.insert( 5, buildEqualToFilter(propertyName="descriptiveGroup", literal=filters['descriptiveGroup']) )
+
+                            elif (element == "make"):
+                                propertyList[6] = True
+                                propertyIsEqualToList.insert( 6, buildEqualToFilter(propertyName="make", literal=filters['make']) )
+
+                    else:
+                        if (element == "featureCode"):
+                            propertyList[1] = True
+                            propertyIsEqualToList.insert( 1, buildEqualToFilter(propertyName="featureCode", literal=filters['featureCode']) )
+
+                        elif (element == "calculatedAreaValue"):
+                            propertyList[3] = True
+                            propertyIsEqualToList.insert( 3, buildEqualToFilter(propertyName="calculatedAreaValue", literal=filters['calculatedAreaValue']) )
+
+                        elif (element == "physicalLevel"):
+                            propertyList[7] = True
+                            propertyIsEqualToList.insert( 7, buildEqualToFilter(propertyName="physicalLevel", literal=filters['physicalLevel']) )
+                    
+            # Create property list inside the And tag
+            for i in range(len(propertyList)):
+                if propertyList[i]:
+
+                    multiplePropertyCount = multiplePropertyCount + 1
+                    multipleProperty = multipleProperty + propertyIsEqualToList[i]
+
+            if (multiplePropertyCount > 1):
+                multipleProperty = "<And>" + multipleProperty + "</And>"
+
+            payload["filter"] = "<Filter>" + multipleProperty + "</Filter>"
+           
     return payload
 
 def fetchFeaturesFromWFS(count, typeNames, filters):
@@ -70,14 +138,21 @@ class Query(graphene.ObjectType):
         literal=graphene.String(default_value="")
     )
 
-    boundaryLinePollingDistrict = graphene.String(
-        first=graphene.Int(default_value=10),
-        ward=graphene.String(default_value="Bottisham Ward"),
-        parish=graphene.String(default_value="Brinkley CP")
-    )
     zoomstackNames = graphene.String(
         first=graphene.Int(default_value=10),
         Name1=graphene.String(default_value="BRECON BEACONS NATIONAL PARK")
+    )
+
+    topographyTopographicArea = graphene.List(graphene.String,
+        first=graphene.Int(),
+        toid=graphene.String(),
+        featureCode=graphene.Int(),
+        theme=graphene.String(),
+        calculatedAreaValue=graphene.Float(),
+        reasonForChange=graphene.String(),
+        descriptiveGroup=graphene.String(),
+        make=graphene.String(),
+        physicalLevel=graphene.Int(),
     )
 
     #   {
@@ -96,22 +171,6 @@ class Query(graphene.ObjectType):
         else:
             return ["Error: Count needs to be 0 or more"]
 
-            
-    # {
-    #      boundaryLinePollingDistrict(
-    #         first: 5,
-    #         ward: "Bottisham Ward",
-    #         parish: "Burrough Green CP"
-    #     )
-    # }
-
-    def resolve_boundaryLinePollingDistrict(self, info, first, ward, parish):
-        filters = {
-            "ward": ward,
-            "parish": parish
-        }
-        return fetchFeaturesFromWFS(count=first, typeNames="osfeatures:BoundaryLine_PollingDistrict", filters=filters)
-
     #  {
     #      zoomstackNames(
     #          first: 5,
@@ -123,6 +182,38 @@ class Query(graphene.ObjectType):
             "Name1": Name1
         }
         return fetchFeaturesFromWFS(count=first, typeNames="osfeatures:Zoomstack_Names", filters=filters)
+
+    # {
+    #      topographyTopographicArea(
+    #         first: 5,
+    #         toid: "osgb1000000000006",
+    #         featureCode: 10021,
+    #         theme: "Buildings",
+    #         calculatedAreaValue: 46.68,
+    #         reasonForChange: "Restructured",
+    #         descriptiveGroup: "Building",
+    #         make: "Manmade",
+    #         physicalLevel: 50,
+    #     )
+    # }
+    def resolve_topographyTopographicArea(self, info, first=1, toid=None, featureCode=None, theme=None, calculatedAreaValue=None, reasonForChange=None, descriptiveGroup=None, make=None, physicalLevel=None):
+        if (first >= 0 or first == None):
+            filters =  {
+                "TOID": toid,
+                "featureCode": featureCode,
+                "theme": theme,
+                "calculatedAreaValue": calculatedAreaValue,
+                "reasonForChange": reasonForChange,
+                "descriptiveGroup": descriptiveGroup,
+                "make": make,
+                "physicalLevel": physicalLevel
+            }
+        
+        else:
+            return ["Error: First needs to be 0 or more"]
+
+        return fetchFeaturesFromWFS(count=first, typeNames="osfeatures:Topography_TopographicArea", filters=filters)
+
 
 
 """HTTP Cloud Function.
--- tests/test_main.py
+++ tests/test_main.py
@@ -78,7 +78,7 @@ class HelloTestCase(unittest.TestCase):
 
     def test_buildWFSQuery_hello(self):
         count = 1
-        typeNames = None
+        typeNames = "osfeatures:Zoomstack_Sites"
         filters = {
             "Type": "Education"
         }
@@ -94,6 +94,123 @@ class HelloTestCase(unittest.TestCase):
                                     <PropertyName>Type</PropertyName>
                                     <Literal>Education</Literal>
                                     </PropertyIsEqualTo>
+                            </Filter>"""
+        stripedExpectedFilter = expectedFilter.replace(' ', '').replace('\n', '')
+
+        self.assertEqual(stripedPayloadFilter, stripedExpectedFilter)
+    
+    @patch('main.requests.get')
+    def test_topographyTopographicArea_one_feature(self, mocked_get):
+        response_data = {"features": ["I'm getting this"]}
+        query = ' { topographyTopographicArea(first: 1) } '
+        expected_value = {'topographyTopographicArea': response_data["features"]}
+        mocked_get.return_value = self.make_mocked_response(response_data)
+        request = self.make_request(query)
+        result = graphqlwfs(request)
+
+        self.assertEqual(result, expected_value)
+
+    @patch('main.requests.get')
+    def test_topographyTopographicArea_two_features(self, mocked_get):
+        response_data = {"features": ["I'm getting this 1", "I'm getting this 2"]}
+        query = ' { topographyTopographicArea(first: 2) } '
+
+        expected_value = {'topographyTopographicArea': response_data["features"]}
+        mocked_get.return_value = self.make_mocked_response(response_data)
+        request = self.make_request(query)
+        result = graphqlwfs(request)
+
+        self.assertEqual(result, expected_value)
+    
+    @patch('main.requests.get')
+    def test_topographyTopographicArea_empty_filter_parameters(self, mocked_get):
+        response_data = {"features": ["I'm getting this 1", "I'm getting this 2"]}
+        query = ' { topographyTopographicArea(first: 2, theme: "", descriptiveGroup: " ") } '
+
+        expected_value = {'topographyTopographicArea': response_data["features"]}
+        mocked_get.return_value = self.make_mocked_response(response_data)
+        request = self.make_request(query)
+        result = graphqlwfs(request)
+
+        self.assertEqual(result, expected_value)
+
+    @patch('main.requests.get')
+    def test_topographyTopographicArea_counter_negative(self, mocked_get):
+        response_data = 'Error: First needs to be 0 or more'
+        query = ' { topographyTopographicArea(first: -5) } '
+
+        expected_value = {'topographyTopographicArea': [response_data]}
+        mocked_get.return_value = self.make_mocked_response(response_data)
+        request = self.make_request(query)
+        result = graphqlwfs(request)
+
+        self.assertEqual(result, expected_value)
+
+    def test_topographyTopographicArea_buildWFSQuery_single_filter(self):
+        first = 1
+        typeNames = "osfeatures:Topography_TopographicArea"
+        filters = {
+            "TOID": "osgb1000000000006",
+            "featureCode": None,
+            "theme": None,
+            "calculatedAreaValue": None,
+            "reasonForChange": None,
+            "descriptiveGroup": None,
+            "make": None,
+            "physicalLevel": None
+        }
+        payload = buildWFSQuery(first, typeNames, filters)
+
+        self.assertEqual(payload['count'], first)
+        self.assertEqual(payload['typeNames'], typeNames)
+
+        stripedPayloadFilter = payload['filter'].replace(
+            ' ', '').replace('\n', '')
+        expectedFilter = """<Filter>
+                                <PropertyIsEqualTo>
+                                    <PropertyName>TOID</PropertyName>
+                                    <Literal>osgb1000000000006</Literal>
+                                    </PropertyIsEqualTo>
+                            </Filter>"""
+        stripedExpectedFilter = expectedFilter.replace(' ', '').replace('\n', '')
+
+        self.assertEqual(stripedPayloadFilter, stripedExpectedFilter)
+
+    def test_topographyTopographicArea_buildWFSQuery_triple_filter(self):
+        first = 1
+        typeNames = "osfeatures:Topography_TopographicArea"
+        filters = {
+            "TOID": "osgb1000000000006",
+            "featureCode": 10021,
+            "theme": None,
+            "calculatedAreaValue": None,
+            "reasonForChange": None,
+            "descriptiveGroup": None,
+            "make": "Manmade",
+            "physicalLevel": None
+        }
+        payload = buildWFSQuery(first, typeNames, filters)
+
+        self.assertEqual(payload['count'], first)
+        self.assertEqual(payload['typeNames'], typeNames)
+
+        stripedPayloadFilter = payload['filter'].replace(
+            ' ', '').replace('\n', '')
+        expectedFilter = """<Filter>
+                                <And>
+                                    <PropertyIsEqualTo>
+                                        <PropertyName>TOID</PropertyName>
+                                        <Literal>osgb1000000000006</Literal>
+                                    </PropertyIsEqualTo>
+                                    <PropertyIsEqualTo>
+                                        <PropertyName>featureCode</PropertyName>
+                                        <Literal>10021</Literal>
+                                    </PropertyIsEqualTo>
+                                    <PropertyIsEqualTo>
+                                        <PropertyName>make</PropertyName>
+                                        <Literal>Manmade</Literal>
+                                    </PropertyIsEqualTo>
+                                </And>
                             </Filter>"""
         stripedExpectedFilter = expectedFilter.replace(' ', '').replace('\n', '')
 
